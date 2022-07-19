e3164f76b60f73189802629155eead7a1f7bc1b8
cra123 <51616589+cra123@users.noreply.github.com>
2022-07-19 16:41:00 +0200

    done

--- js/index.js
+++ js/index.js
@@ -1,6 +1,46 @@
 // Iteration 1: Names and Input
-//
+let hacker1= "Mohamed"
+console.log("the driver's name is " + hacker1)
+
 // Iteration 2: Conditionals
+if(hacker1.length > hacker2.length) {
+  console.log("The driver has the longest name,it has " + hacker1.length + "
+  characters. ")
+}
+
+if (hacker1.length < hacker2.length) {
+  console.log("It seems that the navigator has the longerst name it has" + hacker1.length + " characters.")
+
+  }
+
+  if( hacker1.length == hacker2.len) {
+    console.log("It seems that the navigator has the longest name,it has " + hacker1.length + " characters.")
+  }
+
+
 
 
 // Iteration 3: Loops
+// print all the the characters of the driver's name separated by a space and 
+let new_hacker1 = ""
+for(let 1 = 0; 1 < hacher1.length; i++) {
+  if (i == hacker1.length - 1){
+    new_hacker1 = new_hacker1 + hacker1 [i]. toUpperCase();
+  }
+  else {
+    new_hacker1 = new_hacker1 + hacker1[i].toUpperCase() + " ";
+  }
+}
+console.log(new_hacker1)
+// print all the characters of the navigator's name, in reverse order.
+let new_hacker2 = ""
+for( let i = hacker1.length - 1; i >=0 ; i- -) {
+  if ( i == 0){
+    new_hacker2 = new_hacker2 + hacker1[i].sup();
+  }
+  else{
+    new_hacker2 = new_hacker2 + hacker1[i].toLowerCase() +" ";
+  }
+}
+console.log (new_hacker2)
+/// Iteration 1: Names and input
